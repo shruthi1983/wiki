47e5fdc2f9aa33687f6f59f1406f6688edd85957
shruthi <shruthirmgm@gmail.com>
2021-12-17 13:54:35 +0530

    created category migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_065146) do
+ActiveRecord::Schema.define(version: 2021_12_17_082300) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
